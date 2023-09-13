f9fff40200e1938fb6e9240936ee409c752caaa4
Sky Bauerschmidt Sweeney <dbauerschmidtsweeney@gmail.com>
2023-09-13 14:28:20 -0600

    completed lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+class Anagram:
+
+    def __init__(self, string):
+        self.string = string
+
+    def match(self, list):
+        match_list = []
+        for word in list:
+            if sorted(self.string) == sorted(word):
+                match_list.append(word)
+        return match_list
